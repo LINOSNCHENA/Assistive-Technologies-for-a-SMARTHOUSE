f2beb1bda43192bc479dcb5efe7333b6613c8d90
LINOS NCHENA <linosnchena@gmail.com>
2020-01-18 06:02:58 +0100

    SMARTHOUSE ATs

--- Email/zamnet.js
+++ Email/zamnet.js
@@ -1,18 +1,17 @@
 var twilio = require('twilio'),
 // Authentication of twilio account
 
-client = twilio('ACa5b2898f284381b46bc05c14c2c0ff3d', '59dea85a451127e47bb4fff3259bd510'),
+client = twilio('XXXXX', 'XXXX'),
 cronJob = require('cron').CronJob;                     
 
-var numbers = ['+260975631581', '+420774348004','+420773283754',
- '+42077096282','+260979232500', '+18018372837']; 
+var numbers = ['+XXXXX631581', '+XXXXXXXX04','+XXXX4207732']; 
 var sms1 = 'WARNING! FEVER FOR SENIOR CITIZEN #21 HAS ENTERED THE DANGER ZONE!';
 var sms2 = 'WARNING! FEVER FOR SENIOR CITIZEN #49 HAS ENTERED THE DANGER ZONE!';
 
 for( var i = 0; i < numbers.length; i++ ) {
     client.messages.create({ 
       to:numbers[i], 
-      from:'+18558061779', 
+      from:'+XXXX79', 
       body:'Hello ZAMNET Volunteer! '+ sms2}, 
       function( err, data1 ) {console.log('ZAMNET SINGLE #1'+sms2)},
       function( err, data2 ) {console.log('ZAMNET SINGLE #1'+sms1)}
--- Email/zamtel.py
+++ Email/zamtel.py
@@ -8,9 +8,9 @@ sms1 = 'WARNING! FEVER FOR SENIOR CITIZEN #49 HAS ENTERED THE DANGER ZONE!'
 sms2 = 'WARNING! FEVER FOR SENIOR CITIZEN #21 HAS ENTERED THE DANGER ZONE!'
 
 message = client.messages.create(
-         body='| '+ '\n\n'+sms1+'\n\n'+' |',
-         from_='+XX55 806 1779',
-         to='+XXX74348004')
+         body  ='| '+ '\n\n'+sms1+'\n\n'+' |',
+         from_ ='XXXXXXX1779',
+         to    ='+XXXXX348004')
 
 print(message.sid)
 print(message)
